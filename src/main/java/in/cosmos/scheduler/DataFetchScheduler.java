b13cad7993b9ac4f3be3b41dbcfa5f81c6d36208
Yazat2611 <yazatbhardvaj2611@gmail.com>
2025-10-31 08:31:27 +0530

    added cron job to auto refresh fetching of data everyday at 12 am

--- src/main/java/in/cosmos/scheduler/DataFetchScheduler.java
+++ src/main/java/in/cosmos/scheduler/DataFetchScheduler.java
@@ -1,4 +1,27 @@
 package in.cosmos.scheduler;
 
+import in.cosmos.service.AsteroidService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
 public class DataFetchScheduler {
+    private final AsteroidService asteroidService;
+
+    public DataFetchScheduler(AsteroidService asteroidService) {
+        this.asteroidService = asteroidService;
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void fetchData() {
+        try {
+            asteroidService.fetchAndSaveAsteroids();
+            log.info("✅ Scheduler completed successfully");
+        } catch (Exception e) {
+            log.error("Scheduler failed {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
